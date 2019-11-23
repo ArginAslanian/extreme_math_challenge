13518ad25c770aef408d6e88912b9a444216f69d
Argin Aslanian <someone@somewhere.com>
2019-11-23 09:31:27 -0800

    Added Script to check level values and hide game-screen initially

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//alert('Hello, World!');
+/***
+ * Play button actions
+ */
+
+var playButton = document.getElementById('play-button');
+var options = document.getElementsByName('level');
+var gameScreen = document.getElementById('game-screen');
+
+playButton.onclick = function() {
+    if (options[0].checked) {
+        alert('Easy');
+
+    } else if (options[1].checked) {
+        alert('intermediate');
+
+    } else if (options[2].checked) {
+        alert('hard');
+
+    } else {
+        alert('Error: Please select your level!');
+
+    }
+    gameScreen.style.visibility = "visible";
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    gameScreen.style.visibility = "hidden";
+});
