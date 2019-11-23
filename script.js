55767144b3cb97b8cadd339292458b450aba1fa7
Argin Aslanian <someone@somewhere.com>
2019-11-23 09:50:42 -0800

    Added easy, intermediate, hard functions.

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-/***
- * Play button actions
+/**
+ * Levels breakdown:
+ * 
+ * Easy:            Numbers from 0-9
+ * Intermediate:    Numbers from 10-50
+ * Hard:            Numbers from 51-100
  */
 
+ // Variables 
 var playButton = document.getElementById('play-button');
 var options = document.getElementsByName('level');
 var gameScreen = document.getElementById('game-screen');
 
+ // Easy Level
+function easy(min, max) {
+    alert("Easy level selected");
+}
+
+// Intermediate Level
+function intermediate(min, max) {
+    alert("Intermediate level selected");
+}
+
+// Hard Level
+function hard(min, max) {
+    alert("Hard level selected");
+}
+
+
+/***
+ * Play button actions
+ */
+
 playButton.onclick = function() {
     if (options[0].checked) {
-        alert('Easy');
+        easy(0, 9);
 
     } else if (options[1].checked) {
-        alert('intermediate');
+        intermediate(10, 50);
 
     } else if (options[2].checked) {
-        alert('hard');
+        hard(51, 100);
 
     } else {
         alert('Error: Please select your level!');
